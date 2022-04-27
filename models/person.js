ae433721ec3050fbc3d72b2d47569fe68366dc31
asianomainen <oskari.nuottonen@gmail.com>
2022-04-27 23:31:55 +0300

    Exercise 3.22

--- models/person.js
+++ models/person.js
@@ -5,38 +5,38 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('Error connecting to MongoDB', error.message)
-    })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number! A valid phone number consists of 2 or 3 numbers followed by a hyphen (-) followed by any amount of numbers.`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! A valid phone number consists of 2 or 3 numbers followed by a hyphen (-) followed by any amount of numbers.`
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
